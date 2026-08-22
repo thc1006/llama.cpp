a1aac6383b0f8f58ac9085294308fbb0defb73f7
Aleksander Grygier <aleksander.grygier@gmail.com>
2026-08-22 09:55:00 +0200

    feat(ui): make base dialog responsive and support sticky headers

--- tools/ui/src/lib/components/ui/dialog/dialog-header.svelte
+++ tools/ui/src/lib/components/ui/dialog/dialog-header.svelte
@@ -13,7 +13,10 @@
 <div
 	bind:this={ref}
 	data-slot="dialog-header"
-	class={cn('flex flex-col gap-2 text-center sm:text-left', className)}
+	class={cn(
+		'flex flex-col gap-2 text-center sm:text-left sticky top-0 z-50 bg-background md:bg-transparent',
+		className
+	)}
 	{...restProps}
 >
 	{@render children?.()}
